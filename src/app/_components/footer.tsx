403d638d172bf8b3cb2cb9a167cc6058a7e2b03d
jngmnj <exorbitance@naver.com>
2025-09-11 02:57:31 +0900

    style: 푸터 레이아웃 및 디자인 개편
    - 불필요한 문구 및 문서 링크 제거
    - Container 제거로 마크업 단순화
    - GitHub / Instagram 아이콘 링크 추가 (remixicon 사용)
    - 어두운 배경색 + 흰색 텍스트로 스타일 변경
    - 현재 연도 자동 표기 기능 추가

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,30 +1,25 @@
-import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
+import { RiGithubLine, RiInstagramLine } from '@remixicon/react';
+import Link from 'next/link';
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+    <footer className="bg-black py-8">
+      <div className="flex items-center justify-center">
+        <Link href="https://github.com/jngmnj" target="_blank">
+          <div className="p-4">
+            <RiGithubLine className="size-6 text-white" />
           </div>
-        </div>
-      </Container>
+        </Link>
+        <Link href="https://instagram.com/jngmnj" target="_blank">
+          <div className="p-4">
+            <RiInstagramLine className="size-6 text-white" />
+          </div>
+        </Link>
+      </div>
+      <div className="">
+        <p className="text-center text-white">&copy; {currentYear} jngmnj</p>
+      </div>
     </footer>
   );
 }
